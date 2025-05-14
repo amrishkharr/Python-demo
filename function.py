1942953a1b23406f4902d73f0afb700eca19177b
Amrishkhar <130848126+amrishkharr@users.noreply.github.com>
2025-05-14 11:45:46 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,20 @@
+def printing(y):
+    print("hello",y)
+    print("hello")
+    print("hello")
+    print("hello")
+    print("hello")
+    print("hello")
+    print("hello")
+    print("hello")
+    print("hello")
+    print("hello")
+    
+
+
+printing(5)
+
+# with arugmrnt without return 
+# without arugmrnt with return 
+# without arugmrnt without return 
+# with arugmrnt with return 
